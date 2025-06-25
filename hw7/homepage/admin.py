68dad40814308868b4bad9424eda90b7c4649f9c
Arnone215 <tarnone21589@gmail.com>
2025-06-24 21:38:09 -0700

    HW11 completed and working properly!!!

--- hw7/homepage/admin.py
+++ hw7/homepage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("title", "genre", "platform")  # 👈 Custom columns in list view
+    search_fields = ("title", "genre")  # 👈 Allows admin to search these fields
+    list_filter = ("platform",)  # 👈 Adds a filter sidebar for this field
